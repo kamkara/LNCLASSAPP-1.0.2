bad648942833ed494d4932ef1b5c6f80b2db4c31
kamkara <lnclass.kamkara@gmail.com>
2021-01-18 22:51:34 +0000

    materials and add shared content'course, sidebar,nav and users'

---
+++ app/models/material.rb
@@ -0,0 +1,11 @@
+class Material < ApplicationRecord
+  belongs_to :user
+
+  ############ SLUG ###########
+  extend FriendlyId
+    friendly_id :title, use: :slugged
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
+end
