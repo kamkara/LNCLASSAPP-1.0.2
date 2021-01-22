f121df42e75e09d7777c232f8da64f37b3a8d010
kamkara <lnclass.kamkara@gmail.com>
2021-01-22 13:58:27 +0000

    build user profil

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,25 @@
 class DashboardController < ApplicationController
+  
+  before_action :authenticate_user!
+  layout "dashboard"
+  before_action :find_materials
+  before_action :find_levels
+
   def index
+    @materials = Material.all
+    @levels = Level.all
+  end
+
+
+private
+
+  #enable material
+  def find_materials
+    @materials = Material.all
+  end
+
+  #enable level
+  def find_levels
+    @levels = Level.all
   end
 end
