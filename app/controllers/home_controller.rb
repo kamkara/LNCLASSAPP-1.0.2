76d5c3c8c6757ad6cf5ad80008c3311c8ff70fe4
kamkara <lnclass.kamkara@gmail.com>
2021-01-23 14:38:46 +0000

    Dahboard build, Course navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,11 @@ class HomeController < ApplicationController
   before_action :find_materials
 
   def index
-    @courses = Course.all.order('created_at desc')
+    if current_user.role == "student"
+      @courses = Course.where("course.level_id=?", current_user.id).order('created_at desc')
+    else current_user.role == "teacher" || current_user.role == "team"
+      @courses   = Course.all.order('created_at desc')
+    end
     @materials = Material.all.order('created_at desc')
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,6 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+
 end
