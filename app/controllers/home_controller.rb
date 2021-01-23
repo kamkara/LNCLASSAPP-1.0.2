169e0505abc746ec7eb8c100df5a32d0e3dcc3c7
kamkara <lnclass.kamkara@gmail.com>
2021-01-23 22:31:20 +0000

    fash message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,28 @@
 class HomeController < ApplicationController
+  
+  #before_action :authenticate_user!
+  #before_action :authenticate_user!, unless: :current_user_connected?
   before_action :find_levels
   before_action :find_materials
 
-  def index
-    if current_user.role == "student"
-      @courses = Course.where("course.level_id=?", current_user.id).order('created_at desc')
-    else current_user.role == "teacher" || current_user.role == "team"
+
+  #def current_user_connected?
+  #  def index
+  #    if current_user.role == "student"
+  #      @courses = Course.where("course.level_id =? ", current_user.level_id).order("created_at desc")
+  #      @materials = Material.all.order("created_at desc")
+  #    elsif current_user.role == "teacher" || current_user.role == "team"
+  #      @courses   = Course.all.order('created_at desc')
+  #      @materials = Material.all.order('created_at desc')
+  #    end
+  #  end
+  #end
+  
+    def index
       @courses   = Course.all.order('created_at desc')
+      @materials = Material.all.order('created_at desc')
     end
-    @materials = Material.all.order('created_at desc')
-  end
+ 
 
 
 
