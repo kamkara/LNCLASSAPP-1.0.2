dab890f7b31a05d6b291c8031d36ad631c096b77
kamkara <lnclass.kamkara@gmail.com>
2021-01-23 16:18:53 +0000

    add rediction with loclae after authentication failure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    
+   #Redirect with locale after authentication failure 
+   def set_locale
+      I18n.locale = params[:locale]
+    end
+
+    def self.default_url_options(options={})
+      options.merge({ :locale => I18n.locale })
+    end 
+# Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
