79e6eac19e6e5286bc1c227b89c65cf00d1ade72
kamkara <lnclass.kamkara@gmail.com>
2021-03-11 10:29:50 +0000

    actionText

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("channels")
 // RICH_TEXT
 require("trix")
 require("@rails/actiontext")
+
 //= require jquery
 
 // Uncomment to copy all static images under ../images to the output folder and reference
